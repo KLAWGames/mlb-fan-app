ac5c2fe84cae2a629e5c428454b1e9262c6781d6
Baseball Fan <fan@mlb-fanapp.com>
2026-06-25 16:38:43 -0600

    Chore: Add vite.config.js boilerplate for GitHub Pages deployment option

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // If you use GitHub Pages, uncomment the line below and change 'mlb-fan-app' to match your GitHub repository name:
+  // base: '/mlb-fan-app/',
+});
